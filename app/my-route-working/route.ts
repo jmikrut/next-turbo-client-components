d2ef7d2cc4ec6a3c4c5f9cae597f060a86867e58
James <james@trbl.design>
2024-04-22 11:17:38 -0400

    chore: updates reproduction to show that node_modules are still broken

--- app/my-route-working/route.ts
+++ app/my-route-working/route.ts
@@ -3,8 +3,9 @@ import { ClientComponent } from "../ClientComponent"
 export const GET = () => {
   // This import works in Webpack, and used to work in Turbopack.
 
-  // But any versions after 14.2.0-canary.33 (i.e. 14.2.0) now break Turbopack,
-  // even in spite of the errored file indeed having 'use client' at the top.
+  // Looks like it was fixed in 14.3.0-canary.8.
+  // Corresponding GitHub issue:
+  // https://github.com/vercel/next.js/issues/64412
 
   return Response.json({
     typeofClientComponent: typeof ClientComponent,
